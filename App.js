4b7b65ddc7abbd7c2f8185265a76fb3a1f298b21
Asad Ali <asadalihp@gmail.com>
2018-04-04 17:30:17 +0500

    New Realease
    All processor architecture and devises support added for Android. and Performance improvements for ios

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <WebView
+        source={{uri: 'http://cliente.dayback.com.br'}}
+        style={{marginTop: 20}}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
